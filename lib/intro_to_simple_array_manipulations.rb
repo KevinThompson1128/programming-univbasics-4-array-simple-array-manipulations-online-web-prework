3d63900282554fde50522901406367b5b536d13f
Kevin Thompson <indelible11@yahoo.com>
2019-10-24 14:29:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,9 +29,7 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, string)
-  list_of_programming_languages = "Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"
-  another_language = "Python"
-  list_of_programming_languages.insert(3, "Python")
+  array.insert()
 end
 
 def using_uniq(array)
